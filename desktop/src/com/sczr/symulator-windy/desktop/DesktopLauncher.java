648d4188a901028e823b9e2be47b7e8a5f93f81b
stratocumulus <kuba10@gmail.com>
2014-12-14 12:19:38 +0100

    Initial commit

---
+++ desktop/src/com/sczr/symulator-windy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sczr.symulator-windy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sczr.symulator-windy.SCZRApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SCZRApplication(), config);
+	}
+}
