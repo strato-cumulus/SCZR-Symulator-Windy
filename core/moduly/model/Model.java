495925d1b2b27668da5554e16415d9b1133d0105
arap <conwenans@gmail.com>
2015-01-24 19:26:05 +0100

    as

--- core/moduly/model/Model.java
+++ core/moduly/model/Model.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import model.elevator.ElevatorCar;
 
@@ -8,6 +10,9 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 import com.sczr.symulator_windy.packets.ElevatorCallPacket;
+import com.sczr.symulator_windy.packets.GUIpackets.ElevatorStateInfoPacket;
+import com.sczr.symulator_windy.packets.GUIpackets.GUIRegisterPacket;
+import com.sczr.symulator_windy.packets.GUIpackets.InitializeGUIPacket;
 import com.sczr.symulator_windy.packets.passengerpackets.NewPassengerPacket;
 import com.sczr.symulator_windy.serialization.SerializationList;
 
@@ -18,10 +23,13 @@ import com.sczr.symulator_windy.serialization.SerializationList;
 public class Model{
 	public static final int NUMBER_OF_FLOORS = 10;
 	public static final int FLOOR_HEIGHT = 90;
+	public static final int GUI_REFRESH_RATE = 100;
 
 	private final Server server;
 	private final ElevatorCar elevatorCar = new ElevatorCar(NUMBER_OF_FLOORS, FLOOR_HEIGHT);
 	private Floor[] floors = new Floor[NUMBER_OF_FLOORS];
+	
+	private int GUIConnectionId = -1;
 	
 	
 	public Model(int tcpPort) throws IOException{
@@ -45,18 +53,43 @@ public class Model{
 				//modul sterowania
 				
 				//modul gui
+				else if(o instanceof GUIRegisterPacket){
+					GUIConnectionId = c.getID();
+					c.sendTCP(new InitializeGUIPacket(NUMBER_OF_FLOORS, FLOOR_HEIGHT));
+				}
 				//else if(o instanceof ElevatorCa)
 
 			}
 		});
 		
+		
+		
 		SerializationList.register(server.getKryo());
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				server.sendToTCP(GUIConnectionId, 
+						new ElevatorStateInfoPacket(elevatorCar.getCurrentDoorWidth(), elevatorCar.getCurrentHeight()));
+			}
+		}, GUI_REFRESH_RATE);
+		
 	}
 	
 	public static void main(String[] args){
 		System.out.println("...GÓRAL...");
 		Model model; 
-		try{model = new Model(1234);}catch(Exception e){}
+		
+		
+		try{
+			model = new Model(1234);
+		}
+		catch(Exception e){
+			e.printStackTrace(); 
+			System.exit(1);
+		}
+		
 	}
 
 }
---
+++ core/src/com/sczr/symulator_windy/packets/GUIpackets/InitializeGUIPacket.java
@@ -0,0 +1,12 @@
+package com.sczr.symulator_windy.packets.GUIpackets;
+
+public class InitializeGUIPacket {
+	Integer storeyNumber;
+	Integer storeyHeight;
+	
+	public InitializeGUIPacket(int storeyNumer, int storeyHeight){
+		this.storeyHeight = storeyHeight;
+		this.storeyNumber = storeyNumber;
+	}
+	
+}
