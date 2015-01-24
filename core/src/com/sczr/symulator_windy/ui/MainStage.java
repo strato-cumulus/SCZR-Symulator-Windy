9065dcc8898fabd1e79a55786f441336dcaa820a
stratocumulus <kuba10@gmail.com>
2015-01-24 23:29:54 +0100

    bhy

--- core/src/com/sczr/symulator_windy/ui/MainStage.java
+++ core/src/com/sczr/symulator_windy/ui/MainStage.java
@@ -102,7 +102,7 @@ public class MainStage extends Stage
 				public void clicked(InputEvent event, float x, float y){
 					Random random = new Random();
 					//peopleWaitingOnStorey[Integer.parseInt(event.getListenerActor().getName())]+=1;
-					client.sendTCP(new NewPassengerPacket(lastClientID++, Integer.parseInt(event.getListenerActor().getName()), random.nextInt()));
+					client.sendTCP(new NewPassengerPacket(lastClientID++, Math.abs(random.nextInt() % stories), Integer.parseInt(event.getListenerActor().getName())));
 				}
 			});
 			
