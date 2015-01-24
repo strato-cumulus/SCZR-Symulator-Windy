f1261ebbea1a7a8692a2735dbde04d4a45b566a6
dobrzynskab <dobrzynskab@gmail.com>
2015-01-24 18:05:43 +0100

    xxxxxx

--- core/moduly/passengersmodule/RandomCreatingOfPassengers.java
+++ core/moduly/passengersmodule/RandomCreatingOfPassengers.java
@@ -4,13 +4,15 @@ import java.util.Random;
 
 public class RandomCreatingOfPassengers {
 
+	Random rand = new Random();
+	Random rand2 = new Random();
 	
 	Passenger createRandomPassenger(int LAST_ID)
 	{
 		final int ID = LAST_ID + 1;
-		int floor = Random.nextInt(10);
-		int destination = Random.nextInt(10);
-		if (floor == destination) destination = destination + 3;
+		int floor = rand.nextInt(10);
+		int destination = rand2.nextInt(10);
+		if (floor == destination) destination = destination + 1;
 		
 		Passenger newpassenger = new Passenger(ID,destination,floor);
 		return newpassenger;
